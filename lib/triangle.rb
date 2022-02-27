c5bdb657a3a1077a147cca35e1f73f7ea467e36e
Chun Yi <8.5by11@gmail.com>
2022-02-27 14:06:34 -0500

    commit tree 73b31c8671bbd8fc8baf6e9abb9619af9858983a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_accessor :side_a, :side_b, :side_c, :kind
+
+  def initialize(side_a, side_b, side_c)
+    self.side_a = side_a
+    self.side_b = side_b
+    self.side_c = side_c
+  end
+
+  def kind
+    if (side_a * side_b * side_c) == 0 || (side_a + side_b) <= side_c || (side_b + side_c) <= side_a || (side_c + side_a) <= side_b
+      raise TriangleError
+    elsif (side_a==side_b and side_a == side_c)
+      self.kind = :equilateral
+    elsif (side_a==side_b || side_a==side_c || side_b==side_c)
+        self.kind = :isosceles
+    else
+      self.kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
